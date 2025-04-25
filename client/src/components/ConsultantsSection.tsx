ee6404ee186858f839d786f97bbf8ba4ac241482
akshayvp1 <akshayachu2427@gmail.com>
2025-04-25 12:16:52 +0530

    add new buttons

--- client/src/components/ConsultantsSection.tsx
+++ client/src/components/ConsultantsSection.tsx
@@ -42,6 +42,7 @@ const sectionVariants = {
   hidden: { opacity: 0, y: 60 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.9, ease: 'easeOut' } },
 };
+console.log(("ddd"));
 
 const ConsultantsSection: React.FC = () => {
   return (
